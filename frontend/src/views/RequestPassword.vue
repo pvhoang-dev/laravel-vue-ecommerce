12e8827eadd18a2c803f8bd5fbfc0464dba1b936
bino <hoangpham23052003@gmail.com>
2024-04-12 09:58:54 +0700

    Create RequestPassword.vue and ResetPassword.vue forms

--- frontend/src/views/RequestPassword.vue
+++ frontend/src/views/RequestPassword.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import { LockClosedIcon } from "@heroicons/vue/24/solid";
+</script>
 
 <template>
   <div
@@ -7,7 +9,7 @@
     <div class="max-w-md w-full space-y-8">
       <div>
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Sign in to your account
+          Request new password
         </h2>
       </div>
       <form class="mt-8 space-y-6" action="#" method="POST">
@@ -21,43 +23,19 @@
               type="email"
               autocomplete="email"
               required=""
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder="Email address"
-            />
-          </div>
-          <div>
-            <label for="password" class="sr-only">Password</label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              autocomplete="current-password"
-              required=""
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Password"
             />
           </div>
         </div>
 
         <div class="flex items-center justify-between">
-          <div class="flex items-center">
-            <input
-              id="remember-me"
-              name="remember-me"
-              type="checkbox"
-              class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-            />
-            <label for="remember-me" class="ml-2 block text-sm text-gray-900">
-              Remember me
-            </label>
-          </div>
-
           <div class="text-sm">
             <router-link
-              :to="{ name: 'requestPassword' }"
+              :to="{ name: 'login' }"
               class="font-medium text-indigo-600 hover:text-indigo-500"
             >
-              Forgot your password?
+              Remember your password?
             </router-link>
           </div>
         </div>
@@ -73,12 +51,10 @@
                 aria-hidden="true"
               />
             </span>
-            Sign in
+            Submit
           </button>
         </div>
       </form>
     </div>
   </div>
 </template>
-
-<style scoped></style>
--- frontend/src/views/ResetPassword.vue
+++ frontend/src/views/ResetPassword.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import { LockClosedIcon } from "@heroicons/vue/24/solid";
+</script>
 
 <template>
   <div
@@ -7,57 +9,43 @@
     <div class="max-w-md w-full space-y-8">
       <div>
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Sign in to your account
+          Set new password
         </h2>
       </div>
       <form class="mt-8 space-y-6" action="#" method="POST">
         <input type="hidden" name="remember" value="true" />
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
-            <label for="email-address" class="sr-only">Email address</label>
+            <label for="new-password" class="sr-only">New Password</label>
             <input
-              id="email-address"
-              name="email"
-              type="email"
-              autocomplete="email"
+              id="new-password"
+              name="password"
+              type="password"
               required=""
               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Email address"
+              placeholder="New Password"
             />
           </div>
           <div>
-            <label for="password" class="sr-only">Password</label>
+            <label for="password-repeat" class="sr-only">Repeat Password</label>
             <input
-              id="password"
-              name="password"
+              id="password-repeat"
+              name="password_repeat"
               type="password"
-              autocomplete="current-password"
               required=""
               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Password"
+              placeholder="Repeat Password"
             />
           </div>
         </div>
 
         <div class="flex items-center justify-between">
-          <div class="flex items-center">
-            <input
-              id="remember-me"
-              name="remember-me"
-              type="checkbox"
-              class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-            />
-            <label for="remember-me" class="ml-2 block text-sm text-gray-900">
-              Remember me
-            </label>
-          </div>
-
           <div class="text-sm">
             <router-link
-              :to="{ name: 'requestPassword' }"
+              :to="{ name: 'login' }"
               class="font-medium text-indigo-600 hover:text-indigo-500"
             >
-              Forgot your password?
+              Go back to Login
             </router-link>
           </div>
         </div>
@@ -73,12 +61,10 @@
                 aria-hidden="true"
               />
             </span>
-            Sign in
+            Submit
           </button>
         </div>
       </form>
     </div>
   </div>
 </template>
-
-<style scoped></style>
