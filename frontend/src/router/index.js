fed3237210a46a30f30346f2ca7f26626d0fb2d1
bino <hoangpham23052003@gmail.com>
2024-04-12 18:14:23 +0700

    Implement redirect to login if token does not exist

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,12 +5,16 @@ import Dashboard from "../views/Dashboard.vue";
 import RequestPassword from "../views/RequestPassword.vue";
 import ResetPassword from "../views/ResetPassword.vue";
 import Products from "../views/Products.vue";
+import store from "../store";
 
 const routes = [
   {
     path: "/app",
     name: "app",
     component: AppLayout,
+    meta: {
+      requiresAuth: true,
+    },
     children: [
       {
         path: "dashboard",
@@ -28,22 +32,41 @@ const routes = [
     path: "/login",
     name: "login",
     component: Login,
+    meta: {
+      requiresGuest: true,
+    },
   },
   {
     path: "/request-password",
     name: "requestPassword",
     component: RequestPassword,
+    meta: {
+      requiresGuest: true,
+    },
   },
   {
     path: "/reset-password/:token",
     name: "resetPassword",
     component: ResetPassword,
+    meta: {
+      requiresGuest: true,
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({ name: "login" });
+  } else if (to.meta.requiresGuest && store.state.user.token) {
+    next({ name: "app.dashboard" });
+  } else {
+    next();
+  }
 });
 
 export default router;
