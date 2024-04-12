7e620cc10a2df70ff0c92d6eb2974e6bf211faeb
bino <hoangpham23052003@gmail.com>
2024-04-12 09:49:08 +0700

    Generate Vue application

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, computed } from "vue";
+import store from "../store";
+const test = computed(() => store.state.test);
+
+defineProps({
+  msg: String,
+});
+</script>
+
+<template>
+  {{ test }}
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
