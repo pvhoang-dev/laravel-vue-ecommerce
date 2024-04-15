ee475d3f09bde7a994c4f50256fc94fcb3f5e821
bino <hoangpham23052003@gmail.com>
2024-04-15 20:07:47 +0700

    Activate email verification and update auth views

--- backend/resources/views/auth/confirm-password.blade.php
+++ backend/resources/views/auth/confirm-password.blade.php
@@ -1,11 +1,5 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
+<x-app-layout>
+    <div class="w-[400px] mx-auto">
         <div class="mb-4 text-sm text-gray-600">
             {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
         </div>
@@ -20,10 +14,8 @@
             <div>
                 <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
             </div>
 
             <div class="flex justify-end mt-4">
@@ -32,5 +24,6 @@
                 </x-button>
             </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+        </x-auth-card>
+    </div>
+</x-app-layout>
