d7b648039cbe081795b978938ff9355eeb4bea0c
bino <hoangpham23052003@gmail.com>
2024-04-16 21:26:44 +0700

    Add relations to Customer, CustomerAddress and User models

---
+++ backend/app/Enums/AddressType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum AddressType
+{
+    case Shipping;
+    case Billing;
+}
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -43,4 +43,10 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function customer()
+    {
+        return $this->hasOne(Customer::class);
+    }
 }
