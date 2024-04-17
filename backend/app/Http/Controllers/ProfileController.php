938bdd96c7dc32191496d0290b6926207d109277
bino <hoangpham23052003@gmail.com>
2024-04-17 21:47:41 +0700

    Implement password update from profile form

--- backend/app/Http/Controllers/ProfileController.php
+++ backend/app/Http/Controllers/ProfileController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 
 use App\Enums\AddressType;
+use App\Http\Requests\PasswordUpdateRequest;
 use App\Http\Requests\ProfileRequest;
 use App\Models\Country;
 use App\Models\Customer;
 use App\Models\CustomerAddress;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
 
 class ProfileController extends Controller
 {
@@ -28,26 +31,30 @@ class ProfileController extends Controller
 
     public function store(ProfileRequest $request)
     {
-        $customerData = $request->validated();
-        $shippingData = $customerData['shipping'];
-        $billingData = $customerData['billing'];
-
-        /** @var \App\Models\User $user */
         $user = $request->user();
-        /** @var \App\Models\Customer $customer */
         $customer = $user->customer;
-        $shippingAddress = $customer->shippingAddress ?: new CustomerAddress(['type' => AddressType::Shipping]);
-        $billingAddress = $customer->billingAddress ?: new CustomerAddress(['type' => AddressType::Billing]);
-        $customer->update($customerData);
 
-        $shippingAddress->fill($shippingData);
-        $shippingAddress->customer_id =  $user->id;
-        $billingAddress->fill($billingData);
-        $billingAddress->customer_id =  $user->id;
-        $shippingAddress->save();
-        $billingAddress->save();
+        $customer->update($request->validated());
+
+        $customer->shippingAddress->updateOrCreate(['customer_id' => $user->id, 'type' => AddressType::Shipping], $request->input('shipping'));
+        $customer->billingAddress->updateOrCreate(['customer_id' => $user->id, 'type' => AddressType::Billing], $request->input('billing'));
 
         $request->session()->flash('flash_message', 'Profile was successfully updated.');
+
+        return redirect()->route('profile');
+    }
+
+    public function passwordUpdate(PasswordUpdateRequest $request)
+    {
+        /** @var \App\Models\User $user */
+        $user = $request->user();
+
+        $passwordData = $request->validated();
+
+        $user->password = Hash::make($passwordData['new_password']);
+        $user->save();
+
+        $request->session()->flash('flash_message', 'Your password was successfully updated.');
 
         return redirect()->route('profile');
     }
