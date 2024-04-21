6e3587926432244acc36d189073a9e79014f1763
bino <hoangpham23052003@gmail.com>
2024-04-21 21:54:53 +0700

    Implement searching for products on website

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -4,41 +4,49 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::query()
-            ->where('published', '=', 1)
-            ->orderBy('updated_at', 'desc')
-            ->paginate(5);
+        $query = Product::query();
 
-        return view('product.index', [
-            'products' => $products
-        ]);
+        return $this->renderProducts($query);
     }
 
     public function byCategory(Category $category)
     {
         $categories = Category::getAllChildrenByParent($category);
 
-        $products = Product::query()
+        $query = Product::query()
             ->select('products.*')
             ->join('product_categories AS pc', 'pc.product_id', 'products.id')
-            ->where('published', '=', 1)
-            ->whereIn('pc.category_id', array_map(fn ($c) => $c->id, $categories))
-            ->orderBy('updated_at', 'desc')
-            ->paginate(5);
+            ->whereIn('pc.category_id', array_map(fn ($c) => $c->id, $categories));
 
-        return view('product.index', [
-            'products' => $products
-        ]);
+        return $this->renderProducts($query);
     }
 
     public function view(Product $product)
     {
         return view('product.view', ['product' => $product]);
+    }
+
+    private function renderProducts(Builder $query)
+    {
+        $search = \request()->get('search');
+        $products = $query
+            ->where('published', '=', 1)
+            ->where(function ($query) use ($search) {
+                /** @var $query \Illuminate\Database\Eloquent\Builder */
+                $query->where('products.title', 'like', "%$search%")
+                    ->orWhere('products.description', 'like', "%$search%");
+            })->orderBy('updated_at', 'desc')
+            ->paginate(5);
+
+        return view('product.index', [
+            'products' => $products
+        ]);
     }
 }
--- backend/resources/views/product/index.blade.php
+++ backend/resources/views/product/index.blade.php
@@ -5,12 +5,20 @@ $categoryList = \App\Models\Category::getActiveAsTree();
 
 <x-app-layout>
     <x-category-list :category-list="$categoryList" class="-ml-5 -mt-5 -mr-5 px-4" />
+
+    <div class="p-3 pb-0">
+        <form action="" method="GET">
+            <x-input type="text" name="search" placeholder="Search for the products"
+                value="{{ request()->get('search') }}" />
+        </form>
+    </div>
+
     <?php if ($products->count() === 0): ?>
     <div class="text-center text-gray-600 py-16 text-xl">
         There are no products published
     </div>
     <?php else: ?>
-    <div class="grid gap-8 grig-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-5">
+    <div class="grid gap-4 grig-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-3">
         @foreach ($products as $product)
             <!-- Product Item -->
             <div x-data="productItem({{ json_encode([
@@ -22,7 +30,8 @@ $categoryList = \App\Models\Category::getActiveAsTree();
                 'addToCartUrl' => route('cart.add', $product),
             ]) }})"
                 class="border border-1 border-gray-200 rounded-md hover:border-purple-600 transition-colors bg-white">
-                <a href="{{ route('product.view', $product->slug) }}" class="aspect-w-3 aspect-h-2 block overflow-hidden">
+                <a href="{{ route('product.view', $product->slug) }}"
+                    class="aspect-w-3 aspect-h-2 block overflow-hidden">
                     <img :src="product.image" alt=""
                         class="object-cover rounded-lg hover:scale-105 hover:rotate-1 transition-transform" />
                 </a>
