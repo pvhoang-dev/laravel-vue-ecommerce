10dbf21c6d9f5310eaf1e0b581d6f5a89a2b5857
bino <hoangpham23052003@gmail.com>
2024-06-06 09:10:46 +0700

    Up

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -66,7 +66,7 @@ import {
       </span>
       <span class="text-xs"> Customers </span>
     </router-link>
-    <router-link
+    <!-- <router-link
       :to="{ name: 'reports.orders' }"
       class="flex items-center p-2 rounded transition-colors hover:bg-black/30"
     >
@@ -74,6 +74,6 @@ import {
         <ChartBarIcon class="w-5" />
       </span>
       <span class="text-xs"> Reports </span>
-    </router-link>
+    </router-link> -->
   </div>
 </template>
